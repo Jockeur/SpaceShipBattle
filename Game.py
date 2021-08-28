3de8712e10012ca284a3061f26001431fb0c5005
Jockeur <jocceur@gmail.com>
2021-08-29 01:16:26 +0200

    reprise

--- Game.py
+++ Game.py
@@ -5,38 +5,21 @@ class Game:
     def __init__(self, res):
         self.res = res
 
-        self.window_title = "SpaceShips Battle"
-        self.is_running = True
-        # self.clock = pygame.time.Clock()
-
-        self.start()
-
     def start(self):
         pygame.init()
         self.screen = pygame.display.set_mode(self.res)
-        pygame.display.set_caption(self.window_title)
 
-        self.run()
+        self.run
 
     def run(self):
-        while self.is_running:
-            for e in pygame.event.get():
-                self.manage_event(e)
-            self.update()
-        self.quit()
+        pass
 
-    def manage_event(self, e):
-        if e.type == QUIT:
-            self.is_running = False
-        # On va gérer d'autres event
+    def manage_events(self):
+        pass
 
     def update(self):
-        self.screen.fill(50)
-
-        # self.clock.tick(50)
-        pygame.display.flip()
+        pass
 
     def quit(self):
-        pygame.display.quit()
         pygame.quit()
         del self
