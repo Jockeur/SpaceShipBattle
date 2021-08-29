c0db15191db27ae7bc7d7bedb3bbd5a9391eb09b
Jockeur <jocceur@gmail.com>
2021-08-29 22:09:34 +0200

    Tire du Joueur

--- Game.py
+++ Game.py
@@ -1,17 +1,34 @@
 import pygame
 from pygame.locals import *
+from SpaceShip import *
+
 
 class Game:
     def __init__(self, res):
         self.res = res
 
+        self.window_title = "SpaceShips Battle"
         self.is_running = True
+
+        self.clock = pygame.time.Clock()
+
+        self.player_ship_image = pygame.image.load("res/sprites/spaceship.png")
+        self.bullet_image = pygame.image.load("res/sprites/bullet.png")
+
+        self.player = SpaceShip((200, 200), 10, self.player_ship_image, self.bullet_image)
+
+        self.player_bullet_group = pygame.sprite.Group()
 
         self.start()
 
     def start(self):
+        print('Starting...')
         pygame.init()
+        print('Pygame initializated')
         self.screen = pygame.display.set_mode(self.res)
+        print('Window opened')
+        pygame.display.set_caption(self.window_title)
+        print('Initializing player')
 
         self.run()
 
@@ -19,19 +36,51 @@ class Game:
         while self.is_running:
             for e in pygame.event.get():
                 self.manage_events(e)
+            self.manage_pressed_keys()
             self.update()
         self.quit()
 
     def manage_events(self, e):
         if e.type == QUIT:
             self.is_running = False
-        # On va gérer d'autres events
+        if e.type == KEYDOWN:
+            if e.key == K_SPACE:
+                bullet = self.player.fire()
+                if bullet:
+                    self.player_bullet_group.add(bullet)
+
+    def manage_pressed_keys(self):
+        pressed = pygame.key.get_pressed()
+
+        vector = [0, 0]
+        if pressed[K_q] or pressed[K_LEFT]:
+            vector[0] -= 1
+        if pressed[K_d] or pressed[K_RIGHT]:
+            vector[0] += 1
+        if pressed[K_s] or pressed[K_DOWN]:
+            vector[1] += 1
+        if pressed[K_z] or pressed[K_UP]:
+            vector[1] -= 1
+
+        self.player.move(vector[0], vector[1])
+
+    def draw(self):
+        self.screen.blit(self.player.image, self.player.rect)
+        self.player_bullet_group.draw(self.screen)
 
     def update(self):
         self.screen.fill(50)
 
+        self.player.update()
+        self.player_bullet_group.update()
+
+        self.draw()
+
+        self.clock.tick(50)
         pygame.display.flip()
 
     def quit(self):
+        pygame.display.quit()
         pygame.quit()
+        print('Stopping the game')
         del self
---
+++ SpaceShip.py
@@ -0,0 +1,52 @@
+import pygame
+import time
+from Bullet import *
+
+
+class SpaceShip(pygame.sprite.Sprite):
+    def __init__(self, pos, speed, img, bullet_img):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.bullet_img = bullet_img
+        self.bullet_timer = 0.3
+        self.start_time = 0
+        self.can_fire = True
+
+        self.pos = pos
+        self.speed = speed
+        self.angle_speed = 5
+        self.scale = 1.5
+        self.image_origin = img
+
+        self.rotation = 0
+        self.direction = pygame.math.Vector2(0, 1)
+
+        self.image = img
+        self.rect = self.image.get_rect()
+
+    def update(self):
+        self.image = pygame.transform.rotozoom(self.image_origin, self.rotation, self.scale)
+        self.rect = self.image.get_rect()
+        self.rect.center = self.pos
+
+        if time.time() - self.start_time >= self.bullet_timer:
+            self.can_fire = True
+
+    def move(self, x, y):
+        delta_rot =  x*self.angle_speed
+        self.rotation -= delta_rot
+
+        vec = pygame.math.Vector2(0, 1)
+        vec.y = y*self.speed
+        vec.rotate_ip(-self.rotation)
+
+        self.direction.rotate_ip(delta_rot)
+        self.direction.normalize_ip()
+
+        self.pos = (self.pos[0] + vec.x, self.pos[1] + vec.y)
+
+    def fire(self):
+        if self.can_fire:
+            self.can_fire = False
+            self.start_time = time.time()
+            return Bullet(self.pos, (self.direction.x, self.direction.y), 15, self.bullet_img)
